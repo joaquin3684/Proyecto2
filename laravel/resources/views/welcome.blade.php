a55c636263bbb5e3fd679457852688feb2f0d383
joaquin mazoud <joaquinmazoud@gmail.com>
2017-01-15 22:40:32 -0300

    pforro

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelffsdafsd
                 </div>
 
                 <div class="links">
